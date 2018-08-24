000dac7551b73bf84a6c51e9b008e7619fb83241
saikishore143 <33083787+saikishore143@users.noreply.github.com>
2018-08-24 19:29:20 +0530

    test
    test

--- src/main/java/com/fis/perfcoe/ClientApplication.java
+++ src/main/java/com/fis/perfcoe/ClientApplication.java
@@ -3,6 +3,7 @@ package com.fis.perfcoe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 
 @SpringBootApplication
 public class ClientApplication {
