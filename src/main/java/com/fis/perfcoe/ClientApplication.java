ee79af74b762a854682e54d4d538c85f8b0c4d5c
saikishore143 <33083787+saikishore143@users.noreply.github.com>
2018-09-27 21:39:29 +0530

    client
    client

--- src/main/java/com/fis/perfcoe/ClientApplication.java
+++ src/main/java/com/fis/perfcoe/ClientApplication.java
@@ -2,10 +2,6 @@ package com.fis.perfcoe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.web.client.RestTemplate;
-
 
 @SpringBootApplication
 public class ClientApplication {
@@ -13,11 +9,4 @@ public class ClientApplication {
 	public static void main(String[] args) {
 		SpringApplication.run( ClientApplication.class, args);
 	}
-	
-   @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-
-        return builder.build();
-
-    }
 }
