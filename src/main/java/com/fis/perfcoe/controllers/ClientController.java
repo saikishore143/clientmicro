7595d89145c8b4b61167e1c738c99e548ee386de
saikishore143 <33083787+saikishore143@users.noreply.github.com>
2018-09-28 17:04:49 +0530

    removed slash

--- src/main/java/com/fis/perfcoe/controllers/ClientController.java
+++ src/main/java/com/fis/perfcoe/controllers/ClientController.java
@@ -16,14 +16,14 @@ import java.util.Random;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin("*")
+
 public class ClientController {
 	
 	private RestTemplate restTemplate;
 
 	@GetMapping("/getgreeting")
 	public String greeting1() {
-		String url = "http://microservice1-perfcoe-microservice.sdlocpapp.fisdev.local/api/greeting?name=sai/";
+		String url = "http://microservice1-perfcoe-microservice.sdlocpapp.fisdev.local/api/greeting?name=sai";
 	        System.out.println("URL" + url);
 	        //ResponseEntity<String>  emp = restTemplate.getForEntity(url,String.class);
 		  String emp = restTemplate.getForObject(url,String.class);
