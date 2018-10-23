f297bbcfcaaba0ce1f32a15efd0f5d98fc54df41
saikishore143 <33083787+saikishore143@users.noreply.github.com>
2018-10-23 14:29:47 +0530

    Update ClientController.java

--- src/main/java/com/fis/perfcoe/controllers/ClientController.java
+++ src/main/java/com/fis/perfcoe/controllers/ClientController.java
@@ -23,7 +23,7 @@ public class ClientController {
 
 	@GetMapping("/getgreeting")
 	public String greeting1() {
-		String url = "http://10.45.147.243:8080/api/greeting";
+		String url = "http://microservice-perf-coe.devapps.fisdev.local/api/greeting";
 	        System.out.println("URL" + url);
 	        //ResponseEntity<String>  emp = restTemplate.getForEntity(url,String.class);
 		  String emp = restTemplate.getForObject(url,String.class);
