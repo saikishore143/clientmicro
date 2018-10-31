ffeb8dd57f9ea79107077f2483bf422b7252d84c
saikishore143 <33083787+saikishore143@users.noreply.github.com>
2018-10-31 14:13:25 +0530

    added headers

--- src/main/java/com/fis/perfcoe/controllers/ClientController.java
+++ src/main/java/com/fis/perfcoe/controllers/ClientController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.http.*;
-
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
@@ -24,11 +23,17 @@ public class ClientController {
 	@GetMapping("/getgreeting")
 	public String greeting1() {
 		String url = "http://microservice-perf-coe.devapps.fisdev.local/api/greeting";
-	      // String url = "http://172.30.96.254:8080/api/greeting";
+		 HttpHeaders headers = new HttpHeaders();
+            	 headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+            	 headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
+	         HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+		// String url = "http://172.30.96.254:8080/api/greeting";
 		System.out.println("URL" + url);
 	        //ResponseEntity<String>  emp = restTemplate.getForEntity(url,String.class);
-		  String emp = restTemplate.getForObject(url,String.class);
-	        System.out.println("RESPONSE " + emp);
+		 // String emp = restTemplate.getForObject(url,String.class);
+	        //System.out.println("RESPONSE " + emp);
+		ResponseEntity<String> res = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+       	        System.out.println(res);
 	        return "sai";
 
 	}
