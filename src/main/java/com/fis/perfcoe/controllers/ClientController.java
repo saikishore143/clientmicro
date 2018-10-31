73d4bd8987e0d05c35dbf856b6fb9ff7d99f89c0
saikishore143 <33083787+saikishore143@users.noreply.github.com>
2018-10-31 14:46:43 +0530

    test

--- src/main/java/com/fis/perfcoe/controllers/ClientController.java
+++ src/main/java/com/fis/perfcoe/controllers/ClientController.java
@@ -23,18 +23,18 @@ public class ClientController {
 
 	@GetMapping("/getgreeting")
 	public String greeting1() {
-		String url = "http://microservice-perf-coe.devapps.fisdev.local/api/greeting";
+		//String url = "http://microservice-perf-coe.devapps.fisdev.local/api/greeting";
 		 HttpHeaders headers = new HttpHeaders();
             	 headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
             	 headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
 	         HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
-		// String url = "http://172.30.96.254:8080/api/greeting";
+		 String url = "https://samples.openweathermap.org/data/2.5/weather?id=2172797&appid=b6907d289e10d714a6e88b30761fae22";
 		System.out.println("URL" + url);
 	        //ResponseEntity<String>  emp = restTemplate.getForEntity(url,String.class);
 		 // String emp = restTemplate.getForObject(url,String.class);
 	        //System.out.println("RESPONSE " + emp);
-		ResponseEntity<String> res = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
-       	        System.out.println(res);
+		ResponseEntity<String> res = restTemplate.exchange(url, HttpMethod.GET, entity, Object.class);
+       	       // System.out.println(res);
 	        return "sai";
 
 	}
